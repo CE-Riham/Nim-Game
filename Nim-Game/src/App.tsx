48938536ebd77a7047417491e6b81d807296752a
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2023-11-17 17:32:11 +0200

    edit welcomePage, versionsPage, add configPage

--- Nim-Game/src/App.tsx
+++ Nim-Game/src/App.tsx
@@ -3,25 +3,47 @@ import WelcomePage from "./pages/WelcomePage";
 import VersionsPage from "./pages/VersionsPage";
 import VersionOne from "./pages/VersionOne";
 import VersionTwo from "./pages/VersionTwo";
+import ConfigurationsPage from "./pages/ConfigPage";
+import ConfigPage from "./pages/ConfigPage";
+
+export type Game = {
+  numberOfPiles: number[];
+  version: string | null;
+  player2: string;
+  difficulty: string;
+};
+
+export const gameDefault: Game = {
+  numberOfPiles: [],
+  version: null,
+  player2: "computer",
+  difficulty: "easy",
+};
+
 function App() {
   const [currentPage, setCurrentPage] = useState("WelcomePage");
-  const [prevPage, setPrevPage] = useState("WelcomePage");
+  const [gameSettings, setGameSettings] = useState<Game>(gameDefault);
+  console.log(gameSettings);
 
   return (
     <div style={{ position: "absolute", width: "100%", height: "100%" }}>
       {/* <VersionsPage></VersionsPage> */}
+      {/* {<ConfigurationsPage></ConfigurationsPage>} */}
       {currentPage === "WelcomePage" && (
-        <WelcomePage
-          onClick={() => {
-            setCurrentPage("VersionsPage");
-          }}
-        />
+        <WelcomePage setCurrentPage={setCurrentPage} />
       )}
       {currentPage === "VersionsPage" && (
-        <VersionsPage setCurrentPage={setCurrentPage} />
+        <VersionsPage
+          setCurrentPage={setCurrentPage}
+          setGameSettings={setGameSettings}
+        />
       )}
-      {currentPage === "VersionOne" && <VersionOne />}
-      {currentPage === "VersionTwo" && <VersionTwo />}
+      {currentPage === "ConfigPage" && (
+        <ConfigPage
+          setCurrentPage={setCurrentPage}
+          setGameSettings={setGameSettings}
+        />
+      )}
     </div>
   );
 }
